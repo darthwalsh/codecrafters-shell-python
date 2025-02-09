cf863507961acd275070b08d413354d716094dc6
Carl Walsh <darthwalsh@gmail.com>
2025-02-08 23:26:39 -0800

    invalid input

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ import sys
 def main():
     sys.stdout.write("$ ")
 
-    # Wait for user input
-    input()
+    command = input()
+    print(f"{command}: command not found")
+    exit(1)
 
 
 if __name__ == "__main__":
